93149d9d5f21160036c36bc660de4deaef107b64
Alejandro Franco <jafrancov@gmail.com>
2019-11-25 15:39:56 -0600

    06 ngFor & ngIf

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
-
+  title = 'Video list';
+  // videoList = ['item 1', 'item 2', 'item 3'];
+  videoList = [
+    {name: 'item 1', slug: 'item-1'},
+    {name: 'item 2', slug: 'item-2'},
+    {name: 'item 3', slug: 'item-3'},
+  ];
   constructor() { }
 
   ngOnInit() {
