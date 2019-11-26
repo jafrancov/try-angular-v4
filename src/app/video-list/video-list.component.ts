7b23c4900d6dd9be79725e9b72c7b87ef8cea49f
Alejandro Franco <jafrancov@gmail.com>
2019-11-26 00:10:26 -0600

    09 Safely Embed a Video

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-list',
@@ -9,12 +10,30 @@ export class VideoListComponent implements OnInit {
   title = 'Video list';
   // videoList = ['item 1', 'item 2', 'item 3'];
   videoList = [
-    {name: 'item 1', slug: 'videos/item-1'},
-    {name: 'item 2', slug: 'videos/item-2'},
-    {name: 'item 3', slug: 'videos/item-3'},
+    {
+      name: 'item 1',
+      slug: 'videos/item-1',
+      embed: 'xNgQOHwsIbg',
+    },
+    {
+      name: 'item 2',
+      slug: 'videos/item-2',
+      embed: 'xNgQOHwsIbg',
+    },
+    {
+      name: 'item 3',
+      slug: 'videos/item-3',
+      embed: null,
+     },
   ];
-  constructor() { }
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
 
   ngOnInit() {
+  }
+
+  getEmbedURL(item) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + item.embed);
   }
 }
