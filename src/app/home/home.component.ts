6277fcf99fbd1dcff6a069f6d34f1f8e2d221a85
Alejandro Franco <jafrancov@gmail.com>
2019-12-16 23:24:38 -0600

    16 Http & Featured

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CarouselConfig } from 'ngx-bootstrap/carousel';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -10,26 +11,42 @@ import { Router } from '@angular/router';
     { provide: CarouselConfig, useValue: { interval: 4500, noPause: true, showIndicators: true } }
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private req: any;
   homeImageList = [
-    {image: 'assets/images/nature/1.jpg', title: 'Image 1', link: '/videos/item-1' },
-    {image: 'assets/images/nature/4.jpg', title: 'Image 4', link: '/videos/item-2' },
-    {image: 'assets/images/nature/5.jpg', title: 'Image 5', link: '/videos/item-3' },
+    // {image: 'assets/images/nature/1.jpg', name: 'Image 1', slug: 'item-1' },
+    // {image: 'assets/images/nature/4.jpg', name: 'Image 4', slug: 'item-2' },
+    // {image: 'assets/images/nature/5.jpg', name: 'Image 5', slug: 'item-3' },
   ];
 
-  constructor(private router: Router) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.req = this.httpClient.get('/assets/json/videos.json').subscribe(data => {
+      console.log(data);
+      // this.homeImageList = data as [any];
+      // console.log(this.videoList[0].name);
+      for (const i of data[Symbol.iterator]()) {
+          if (i.featured) {
+            this.homeImageList.push(i);
+          }
+        }
+    });
   }
 
   preventNormal(event: MouseEvent, image: any) {
     // if (!this.prevented) {
+    if (image.prevented) {
       event.preventDefault();
       // console.log(image.getAttribute('href'));
       // image.setAttribute('href', '/videos');
       // image.link = '/videos';
       // this.prevented = true;
-      this.router.navigate(['/videos']);
-    // }
+      this.router.navigate(['./videos']);
+    }
+  }
+
+  ngOnDestroy() {
+    this.req.unsubscribe();
   }
 }
